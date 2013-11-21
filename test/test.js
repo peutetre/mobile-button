24914171dded25a4f13659c789482d5fea1c89a5
Paul Panserrieu <paul.panserrieu@gmail.com>
2013-11-21 15:08:39 +0100

    oops

--- test/test.js
+++ test/test.js
@@ -2,17 +2,17 @@
  * test.js
  */
 
-var mobile-button = require('../lib/index.js'),
+var MobileButton = require('../lib/index.js'),
     expect = require('expect.js');
 
 onload = function () {
     describe('Testing some stuff', function () {
         describe('First test', function () {
             it('mobile-button must be an function', function () {
-                expect(mobile-button).to.be.an('function');
+                expect(MobileButton).to.be.an('function');
             });
             it('mobile-button must return a the string `this is module mobile-button`', function () {
-                expect(mobile-button()).to.be.equal("this is module mobile-button");
+                expect(MobileButton()).to.be.equal("this is module mobile-button");
             });
         });
     });
