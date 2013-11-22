b35555b3a93fbeff2974d047d9fae22d08c4e5ef
Paul Panserrieu <paul.panserrieu@gmail.com>
2013-11-22 18:52:36 +0100

    touch start example

--- example/example.js
+++ example/example.js
@@ -7,20 +7,33 @@ var Q = require('q'),
     MButton = require('../lib/index.js');
 
 function main() {
-    var btnEl1 = document.getElementById('start1'),
-        ƒ1 = function (evt) {
-            console.log('start touchstart ƒ');
-            return Q.delay(1000).then(function () {
-                console.log('end touchstart ƒ');
-                return;
-            });
+    var log1 = document.getElementById('log1'),
+        log2 = document.getElementById('log2'),
+        btnEl1 = document.getElementById('start1'),
+        btnEl2 = document.getElementById('start2'),
+        ƒ = function (logEl) {
+            return function (evt) {
+                logEl.innerHTML = 'start ƒ';
+                return Q.delay(1000).then(function () {
+                    logEl.innerHTML = 'end ƒ';
+                    Q.delay(1000).then(function () {
+                        logEl.innerHTML = '';
+                    });
+                    return;
+                });
+            }
         },
         btn1 = new MButton.Touchstart({
             el : btnEl1,
-            f : ƒ1
+            f : ƒ(log1)
+        }),
+        btn2 = new MButton.Touchstart({
+            el : btnEl2,
+            f : ƒ(log2)
         });
 
     btn1.bind();
+    btn2.bind();
 }
 
 qstart.then(main).done();
