f3106ca28e641dda4ff19f89162da6ba027eb865
Paul Panserrieu <paul.panserrieu@gmail.com>
2013-11-22 21:51:23 +0100

    default/touchstart test

--- test/default/test-touchstart-button.js
+++ test/default/test-touchstart-button.js
@@ -1,4 +1,5 @@
 var TouchstartBtn = require('../../lib/index').Touchstart,
+    Q = require('q'),
     expect = require('expect.js');
 
 describe('TouchstartButton', function () {
@@ -7,20 +8,88 @@ describe('TouchstartButton', function () {
     });
 
     describe('instance', function () {
-        it.skip('should raise an Error when calling .bind() if already binded', function () {
-
+        it('should raise an Error when calling .bind() if already binded', function () {
+            var btn = new TouchstartBtn({
+                    el : document.createElement('div'),
+                    f : function () { }
+                });
+            btn.bind();
+            expect(function () { btn.bind(); }).to.throwException("Can't bind an already binded button.");
         });
-        it.skip('should raise an Error when calling .unbind() if unbinded', function () {
-
+        it('should raise an Error when calling .unbind() if unbinded', function () {
+            var btn = new TouchstartBtn({
+                    el : document.createElement('div'),
+                    f : function () { }
+                });
+            expect(function () { btn.unbind(); }).to.throwException("Can't unbind a unbinded button.");
         });
-        it.skip('is active until the returned promise of the callback is resolved', function () {
-
+        it('can not be activated if already activated', function (done) {
+            var i = 0,
+                btn = new TouchstartBtn({
+                    el : document.createElement('div'),
+                    f : function () { i = i+1; return Q.delay(200); }
+                });
+            btn.bind();
+            // first fake activation
+            btn.onTouchstart({});
+            setTimeout(function () {
+                // second activation while the first is not done
+                btn.onTouchstart({});
+            },100);
+            setTimeout(function () {
+                expect(i).to.be(1);
+                done();
+            },300);
         });
-        it.skip('can not be activated if already activated', function () {
+        it('should delay 1000 ms the execution of the callback if delay option set to 1000', function (done) {
+            this.timeout(1500);
 
+            var flag = false,
+                btn = new TouchstartBtn({
+                    el : document.createElement('div'),
+                    f : function () { flag = true; },
+                    delay : 1000
+                });
+            btn.bind();
+            // fake activation
+            btn.onTouchstart({});
+            setTimeout(function () {
+                expect(flag).to.be(false);
+            },500);
+            setTimeout(function () {
+                expect(flag).to.be(true);
+                done();
+            },1100);
         });
-        it.skip('should delay 1000 ms the execution of the callback if delay option set to 1000', function () {
+        it('should add the `.active` css class on the underlying DOM element when active', function (done) {
+            var btn = new TouchstartBtn({
+                el : document.createElement('div'),
+                f : function () { return Q.delay(200); }
+            });
 
+            btn.bind();
+            // fake activation
+            btn.onTouchstart({});
+
+            setTimeout(function () {
+                expect(btn.el.classList.contains('active')).to.be(true);
+                done();
+            },100);
+        });
+        it('should remove the `.active` css class on the underlying DOM element when callback ended', function (done) {
+            var btn = new TouchstartBtn({
+                el : document.createElement('div'),
+                f : function () { return Q.delay(100); }
+            });
+
+            btn.bind();
+            // fake activation
+            btn.onTouchstart({});
+
+            setTimeout(function () {
+                expect(btn.el.classList.contains('active')).to.be(false);
+                done();
+            },200);
         });
     });
 });
