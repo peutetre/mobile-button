659eff6e27f4d9cc8336bb18f1590fc989e051be
Paul Panserrieu <paul.panserrieu@gmail.com>
2013-11-27 23:19:11 +0100

    add 2 touchend btns on example

--- example/example.js
+++ example/example.js
@@ -9,8 +9,13 @@ var Q = require('q'),
 function main() {
     var log1 = document.getElementById('log1'),
         log2 = document.getElementById('log2'),
+        log3 = document.getElementById('log3'),
+        log4 = document.getElementById('log4'),
         btnEl1 = document.getElementById('start1'),
         btnEl2 = document.getElementById('start2'),
+        btnEl3 = document.getElementById('end1'),
+        btnEl4 = document.getElementById('end2'),
+        emptyƒ = function () { },
         ƒ = function (logEl) {
             return function (evt) {
                 logEl.innerHTML = 'start ƒ';
@@ -30,10 +35,20 @@ function main() {
         btn2 = new MButton.Touchstart({
             el : btnEl2,
             f : ƒ(log2)
+        }),
+        btn3 = new MButton.Touchend({
+            el : btnEl3,
+            f : ƒ(log3)
+        }),
+        btn4 = new MButton.Touchend({
+            el : btnEl4,
+            f : ƒ(log4)
         });
 
     btn1.bind();
     btn2.bind();
+    btn3.bind();
+    btn4.bind();
 }
 
 qstart.then(main).done();
