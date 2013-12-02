6e4bdecaba9aa455eaa1c77b80dab8145d9188d9
Paul Panserrieu <paul.panserrieu@gmail.com>
2013-12-02 14:31:29 +0100

    default touchend tests

--- test/default/test-touchend-button.js
+++ test/default/test-touchend-button.js
@@ -6,4 +6,94 @@ describe('default/TouchendButton', function () {
     it('must be an function', function () {
         expect(TouchendBtn).to.be.an('function');
     });
+    describe('instance', function () {
+        it('should raise an Error when calling .bind() if already binded', function () {
+            var btn = new TouchendBtn({
+                    el : document.createElement('div'),
+                    f : function () { }
+                });
+            btn.bind();
+            expect(function () { btn.bind(); }).to.throwException("Can't bind an already binded button.");
+        });
+        it('should raise an Error when calling .unbind() if unbinded', function () {
+            var btn = new TouchendBtn({
+                    el : document.createElement('div'),
+                    f : function () { }
+                });
+            expect(function () { btn.unbind(); }).to.throwException("Can't unbind a unbinded button.");
+        });
+        it('should have a setActiveBorder method', function () {
+            var btn = new TouchendBtn({
+                    el : document.createElement('div'),
+                    f : function () { }
+                });
+            expect(btn.setActiveBorder).to.be.an('function');
+        });
+        it('can not be activated if already activated', function (done) {
+            var i = 0,
+                btn = new TouchendBtn({
+                    el : document.createElement('div'),
+                    f : function () { i = i+1; return Q.delay(200); }
+                });
+            btn.bind();
+            // first fake activation
+            btn.onTouchstart({ changedTouches:[{ identifier : 0 }] });
+            btn.onTouchend({ changedTouches:[{ identifier : 0 }] });
+            setTimeout(function () {
+                // second activation while the first is not done
+                btn.onTouchstart({ changedTouches:[{ identifier : 1 }] });
+                btn.onTouchend({ changedTouches:[{ identifier : 1 }] });
+            },50);
+            setTimeout(function () {
+                expect(i).to.be(1);
+                done();
+            },300);
+        });
+       it('should add the `.active` css class on the underlying DOM element when active', function (done) {
+            var btn = new TouchendBtn({
+                el : document.createElement('div'),
+                f : function () { return Q.delay(200); }
+            });
+
+            btn.bind();
+            // fake activation
+            btn.onTouchstart({ changedTouches:[{ identifier : 0 }] });
+            btn.onTouchend({ changedTouches:[{ identifier : 0 }] });
+
+            setTimeout(function () {
+                expect(btn.el.classList.contains('active')).to.be(true);
+                done();
+            },100);
+        });
+        it('should remove the `.active` css class on the underlying DOM element when callback ended', function (done) {
+            var btn = new TouchendBtn({
+                el : document.createElement('div'),
+                f : function () { return Q.delay(100); }
+            });
+
+            btn.bind();
+            // fake activation
+            btn.onTouchstart({ changedTouches:[{ identifier : 0 }] });
+            btn.onTouchend({ changedTouches:[{ identifier : 0 }] });
+
+            setTimeout(function () {
+                expect(btn.el.classList.contains('active')).to.be(false);
+                done();
+            },200);
+        });
+        it('.bind should return the button', function () {
+            var btn = new TouchendBtn({
+                el : document.createElement('div'),
+                f : function () { }
+            });
+            expect(btn.bind()).to.be.equal(btn);
+        });
+        it('.unbind should return the button', function () {
+            var btn = new TouchendBtn({
+                el : document.createElement('div'),
+                f : function () { }
+            });
+            expect(btn.bind().unbind()).to.be.equal(btn);
+        });
+    });
 });
